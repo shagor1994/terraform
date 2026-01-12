184adc687c11740171a2d2b37ec849224d4a8a30
shagor <tarik.imran@bjitgroup.com>
2026-01-13 00:09:56 +0600

    Removed other hands on code

--- aws_provider.tf
+++ aws_provider.tf
@@ -7,16 +7,3 @@ terraform {
   }
 }
 
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-# Alias provider for us-east-1 used by vpc-peering accepter and VPC B
-provider "aws" {
-  alias      = "us_east_1"
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
